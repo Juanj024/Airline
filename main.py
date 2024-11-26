d1dd6a9cae22f46639582bf1f5c2491fec6d77ad
Juanj024 <Juanjok247@gmail.com>
2024-11-25 23:45:29 -0500

    d

--- main.py
+++ main.py
@@ -18,44 +18,47 @@ class Airplane:
 
 
     def selectSeat(self):
-        ticket = 0
-        while ticket != "-1":
+        tickets = 0
+        while tickets != "-1":
             avion.availableSeats()
-            ticket = input("According to the seats below select which one you want to buy (enter -1 to finish)\n")
-            if ticket == -1:
+            tickets = input("According to the seats below select which ones you want to buy (enter -1 to finish)\n")
+            if tickets == -1:
                 print("Thanks for your visit, goodbye")
             else:
-                ticket = int(ticket)
-                if ticket in self._positionSeat:
-                    pos = self._positionSeat.index(ticket) #pos index number of the seat
+                tickets = tickets.split(',')
+                for ticket in tickets:
+                    ticket = int(ticket.strip())
 
-                    if self._seats[pos] == "Open":
-                        if ticket in self._firstClass:
-                            alert = input("This is a first class seat, if you want to enjoy this service theres an extra charge of $30.00\nYou want to continue (yes/no)")
-                            if alert.lower() == "yes":
-                                self._seats[pos] = "Occupied"
-                                specialPrice = self._ticketPrice + self._fee
-                                print()
-                                print(f"Seat {ticket} purchased!! the total is: {specialPrice}")
-                            else:
-                                print("Ok, you can select another seat")
+                    if ticket in self._positionSeat:
+                        pos = self._positionSeat.index(ticket) #pos index number of the seat
 
-                        else:
-                            if ticket in self._emergencyRows:
-                                alert = input("This is a seat next an emergency row!!!\nIf you want to sit here you have to accept that you would be able to help in case of emergency, select (yes/no)")
+                        if self._seats[pos] == "Open":
+                            if ticket in self._firstClass:
+                                alert = input("This is a first class seat, if you want to enjoy this service theres an extra charge of $30.00\nYou want to continue (yes/no)")
                                 if alert.lower() == "yes":
                                     self._seats[pos] = "Occupied"
+                                    specialPrice = self._ticketPrice + self._fee
                                     print()
-                                    print(f"Seat {ticket} purchased!! the total is: {self._ticketPrice}")
+                                    print(f"Seat {ticket} purchased!! the total is: {specialPrice}")
                                 else:
                                     print("Ok, you can select another seat")
+
                             else:
-                                self._seats[pos] = "Occupied"
-                                print(f"Seat {ticket} purchased!! the total is: {self._ticketPrice}")
+                                if ticket in self._emergencyRows:
+                                    alert = input("This is a seat next an emergency row!!!\nIf you want to sit here you have to accept that you would be able to help in case of emergency, select (yes/no)")
+                                    if alert.lower() == "yes":
+                                        self._seats[pos] = "Occupied"
+                                        print()
+                                        print(f"Seat {ticket} purchased!! the total is: {self._ticketPrice}")
+                                    else:
+                                        print("Ok, you can select another seat")
+                                else:
+                                    self._seats[pos] = "Occupied"
+                                    print(f"Seat {ticket} purchased!! the total is: {self._ticketPrice}")
+                        else:
+                            print("Sorry The seat you selected is already occupied :(\nGo ahead and select another one")
                     else:
-                         print("Sorry The seat you selected is already occupied :(\nGo ahead and select another one")
-                else:
-                     print("Sorry you have to select a seat between 1 and 20")
+                        print("Sorry you have to select a seat between 1 and 20")
 
 
 
